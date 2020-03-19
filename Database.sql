9d193ed0bd8d8c4c180dd507479c44fa7c07e1ab
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-03-19 21:32:17 +0530

    Create Database.sql

---
+++ Database.sql
@@ -0,0 +1,16 @@
+Create Database cookbook;
+USE cookbook;
+CREATE TABLE limbs (thing VARCHAR(20), legs INT,arms INT);
+INSERT INTO limbs (things,legs,arms) VALUES ('insect',6,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('human',16,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('fish',10,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('squid',6,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('centipede',100,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('table',4,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('armchair',4,2);
+INSERT INTO limbs (things,legs,arms) VALUES ('photograph',0,1);
+INSERT INTO limbs (things,legs,arms) VALUES ('tripod',3,0);
+INSERT INTO limbs (things,legs,arms) VALUES ('PeglegPete',1,2);
+INSERT INTO limbs (things,legs,arms) VALUES ('space alien',NULL,NULL);
+
+
