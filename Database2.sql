1a21a937035df95111590760a1c795ec041fcc47
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-03-23 11:27:36 +0530

    Create Database2.sql

---
+++ Database2.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS limbs;
+CREATE TABLE limbs
+(
+thing Varchar(20),#what the thing is
+Legs Int,#number of legs it has
+arms Int
+);
+INSERT INTO limbs VALUES('human',2,2);
+INSERT INTO limbs VALUES('insect',6,0);
+INSERT INTO limbs VALUES('squid',0,10);
+INSERT INTO limbs VALUES('fish',0,0);
+INSERT INTO limbs VALUES('centipede',100,0);
+INSERT INTO limbs VALUES('table',4,0);
+INSERT INTO limbs VALUES('armchair',4,2);
+INSERT INTO limbs VALUES('photograph',0,1);
+INSERT INTO limbs VALUES('tripod',3,0);
+INSERT INTO limbs VALUES('Peg Leg Pete',1,2);
+INSERT INTO limbs VALUES('space alien',NULL,NULL);
+
+
+
+
+
+
+
+
+}
